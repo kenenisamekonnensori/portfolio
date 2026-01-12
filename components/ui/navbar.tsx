a3390de6f72b4e12a003e798f65cfdd9ac691f8b
Kenenisa Mekonnen <mekonnenkenenisa2@gmail.com>
2026-01-12 20:08:53 +0300

    edited navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,22 +1,31 @@
-"use client";
+'use client';
 
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { Home, Briefcase, User, Mail } from "lucide-react";
+import { use } from "react";
+import { useRouter } from "next/navigation";
 
 const navItems = [
-    { name: "Home",id: "home", href: "/", icon: Home },
-    { name: "Work",id: "work", href: "/#work", icon: Briefcase },
-    { name: "About",id: "about", href: "/about", icon: User },
-    { name: "Contact",id: "contact", href: "#contact", icon: Mail },
+    { name: "Home",id: "home", href: "/", type: "section", icon: Home },
+    { name: "Work",id: "work", href: "/#work", type: "section", icon: Briefcase },
+    { name: "About",id: "about", href: "/about", type: "route", icon: User },
+    { name: "Contact",id: "contact", href: "#contact", type: "section", icon: Mail },
 ];
 
-const scrollToSection = (id: string) => {
-    document.getElementById(id)?.scrollIntoView({
-        behavior: "smooth",
-    })
-}
+
 export default function Navbar() {
+    const router = useRouter();
+
+    const scrollToSection = (item:{ name: string; id: string; href: string; type: string; icon: any }) => {
+        if (item.type === "section") {
+            document.getElementById(item.id)?.scrollIntoView({
+                behavior: "smooth",
+            });
+        } else if (item.type === "route") {
+            router.push(item.href);
+        }
+    }
     return (
         <motion.nav
             initial={{ y: -24, opacity: 0 }}
@@ -31,7 +40,7 @@ export default function Navbar() {
                             href={item.href}
                             className="group relative inline-flex items-center gap-2 px-3 py-2 rounded-full hover:bg-white/10 transition-colors duration-300"
                             aria-label={item.name}
-                            onClick={() => scrollToSection(item.id)}
+                            onClick={() => scrollToSection(item)}
                         >
                             <item.icon size={20} className="group-hover:text-accent transition-colors" />
                             <span className="hidden sm:inline text-sm uppercase tracking-wider font-bold">{item.name}</span>
